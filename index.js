977b22e95917975888ede79eb3c4c8cf0a37c602
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-05-05 20:36:50 +0500

    main

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let inp = document.querySelector ('.inp')
-let btn1 = document.querySelector ('.o1')
-let btn2 = document.querySelector ('.o2')
-let btn3 = document.querySelector ('.o3')
-let btn4 = document.querySelector ('.o4')
-let btn5 = document.querySelector ('.o5')
-let btn6 = document.querySelector ('.o6')
+let btn = document.querySelector ('.btn')
+let btn2 = document.querySelector ('.btn2')
+let btn3 = document.querySelector ('.btn3')
+let btn4 = document.querySelector ('.btn4')
+let btn5 = document.querySelector ('.btn5')
+let btn6 = document.querySelector ('.btn6')
 let text = document.querySelector(".text")
 let h2 = document.querySelector("h2") 
 
 
 
-btn1.onclick = () => {
+btn.onclick = () => {
     let inpvalue = inp.value
     h2.innerHTML = inpvalue.toUpperCase()
 }
